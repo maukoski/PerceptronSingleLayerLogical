1db55b44f881e92d67ca49bde511644547c05714
William <william_maukoski@hotmail.com>
2023-07-13 12:47:41 -0300

    Correção de erro na construção da matriz, estava sendo construida com o numero de linhas e colunas trocados.

--- Documents/NetBeansProjects/PerceptronSingleLayerLogicalE/src/Model/DataBase.java
+++ Documents/NetBeansProjects/PerceptronSingleLayerLogicalE/src/Model/DataBase.java
@@ -14,7 +14,7 @@ public class DataBase {
 
     //Refatorar esta  classe
     public DataBase(int premiseCount, int[] conclusion) {
-        this.data = new int[premiseCount][(int) Math.pow(2, premiseCount)];
+        this.data = new int[(int) Math.pow(2, premiseCount)][premiseCount];
         for (int i = 0; i < premiseCount; i++) {
             for (int j = 0; j < premiseCount; j++) {
                 data[i][j] = 0;
@@ -25,8 +25,9 @@ public class DataBase {
     public void showData() {
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[i].length; j++) {
-                System.out.println("" + data[i][j]);
+                System.out.print(" " + data[i][j]);
             }
+            System.out.println("");
         }
     }
 
