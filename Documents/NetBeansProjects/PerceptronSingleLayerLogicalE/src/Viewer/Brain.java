01cbf2f7f86b19885ed30b288e621d8ef6a911c0
William <william_maukoski@hotmail.com>
2023-07-16 18:46:39 -0300

    Classe Brain Documentada.

--- Documents/NetBeansProjects/PerceptronSingleLayerLogicalE/src/Viewer/Brain.java
+++ Documents/NetBeansProjects/PerceptronSingleLayerLogicalE/src/Viewer/Brain.java
@@ -19,31 +19,47 @@ public class Brain {
     private double minimumAccuracy;
     private double learningRate;
 
+    
+    /**
+     * Creates a new layer of neurons with a certain number of neurons.
+     *
+     * @param numInputs Number of inputs is used both to define the number of premises and the number of inputs in the neuron.
+     * @param minimumAccuracy Minimum accuracy to stop execution.
+     * @param learningRate The weight update rate.
+     * @param logicalOperation The logical operation being worked on.
+     * @throws IllegalArgumentException If numInputs is less than 2.
+     * zero.
+     **/
     public Brain(int numInputs, double minimumAccuracy, double learningRate, String logicalOperation) {
+        if(numInputs < 2){
+            throw new IllegalArgumentException("The number of inputs should be greater than or equal to 2");
+        }
+        
         this.numInputs = numInputs;
         this.minimumAccuracy = minimumAccuracy;
         this.learningRate = learningRate;
-        int[] conclusion = new int[4];
-
-        conclusion[3] = 1;
 
         this.db = new DataBase(this.numInputs, logicalOperation);
         this.ol = new OutPutLayer(this.numInputs, this.minimumAccuracy, this.learningRate, db);
     }
-    
+    /**
+     * Method responsible for starting the processing.
+     */
     public void startProcess(){
         this.ol.startProcess();
     }
     
+    /**
+     * Method that prints the truth table.
+     */
     public void printTable(){
         this.db.printTable();
     }
     
+    /**
+     * Method that prints only the conclusions of the truth table.
+     */
     public void printConclusion(){
         this.db.getConclusion();
     }
-    
-    
-    
-
 }
